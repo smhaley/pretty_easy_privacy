31d153890f7c7b3d2c8414e6d18de599349cc535
shawn <shawnh87@gmail.com>
2020-10-19 23:25:58 -0400

    updates

---
+++ src/components/PassPhraseConfirm.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const PassPhraseConfirm = (props) => {
+    const status = props.open
+  const [passPhrase, setPassPhrase] = React.useState();
+
+//   const handleClickOpen = () => {
+//     setOpen(true);
+//   };
+
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+
+const passPhraseConfirmBuffer = (e) => {
+    setPassPhrase(e.target.value)
+    console.log(e.target.value)
+}
+
+  return (
+    <div>
+      <Dialog open={status} onClose={props.handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">PassPhrase Confirmation</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please re enter your passphrase below:
+          </DialogContentText>
+          <TextField
+            autoFocus
+            onChange = {passPhraseBuffer}
+            margin="dense"
+            id="name"
+            label="PassPhrase Confirmation"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick= {props.handleConfirm} color="primary">
+            Encrypt
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default PassPhraseConfirm;
--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -5,8 +5,8 @@ import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import FormControl from "@material-ui/core/FormControl";
-import { colors } from "@material-ui/core";
+import PassPhraseConfirm from "./PassPhraseConfirm"
+
 
 const openpgp = require("openpgp");
 
@@ -14,11 +14,7 @@ const useStyles = makeStyles((theme) => ({
   heading: {
     marginTop: "15px",
     marginBottom: "30px",
-
     textAlign: "left",
-  },
-  button: {
-    width: "100px",
   },
 }));
 
@@ -28,6 +24,8 @@ const TextInput = (props) => {
   const [aesInput, aesInputState] = useState();
   const [passPhrase, passPhraseState] = useState();
   const [output, outputState] = useState();
+  const [open, setOpen] = useState(false);
+  const [passPhrase, setPassPhrase] = React.useState();
 
   const handleInput = (e) => {
     aesInputState(e.target.value);
@@ -37,9 +35,8 @@ const TextInput = (props) => {
     passPhraseState(e.target.value);
   };
 
-  let inLineAesSubmit = (e) => {
-    e.preventDefault();
-    console.log("text");
+  let inLineAesSubmit = () => {
+
 
     (async () => {
       const { message } = await openpgp.encrypt({
@@ -53,18 +50,102 @@ const TextInput = (props) => {
     })();
   };
 
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    setOpen(true)
+
+  }
+
   const downloadHandler = () => {
-    console.log('insdie')
-    console.log(output)
     const element = document.createElement("a");
     const file = new Blob([output], { type: "text/plain" });
     element.href = URL.createObjectURL(file);
-    element.download = "test.txt.gpg";
+    element.download = "test.txt.gpg";//make random name
     document.body.appendChild(element);
     element.click();
   };
 
-  // working
+  let handleClose = () =>{setOpen(false)}
+
+  const handleConfirm = (passPhraseConfirm) => {
+    if (passPhraseConfirm===passPhrase){
+      inLineAesSubmit()
+      setOpen(false)
+    }
+  }
+
+  const passPhraseConfirmBuffer = (e) => {
+    setPassPhrase(e.target.value)
+    console.log(e.target.value)
+}
+
+
+
+  console.log(passPhrase)
+  
+  return (
+    <Grid container wrap="nowrap" spacing={0}>
+      <Grid item></Grid>
+      <Grid item xs>
+        <Typography className={classes.heading} variant="h5" gutterBottom>
+          In-Line Pass Phrase
+        </Typography>
+
+        <form onSubmit={handleSubmit}>
+          <Box mb={2}>
+            <TextField
+              onChange={handlePassPhrase}
+              className={classes.pwText}
+              id="pw-in"
+              label="Pass Phrase"
+              variant="outlined"
+            />
+          </Box>
+          <Box mb={2} mr={2}>
+            <TextField
+              className={classes.textBox}
+              fullWidth={true}
+              id="outlined-multiline-static"
+              label="AES 256"
+              multiline
+              rows={10}
+              onChange={handleInput}
+              variant="outlined"
+            />
+          </Box>
+          <Box mb={2}>
+            <Button
+              type="submit"
+              variant="contained"
+            >
+              Encrypt
+            </Button>
+          </Box>
+          <Box>
+            <Button
+              onClick={downloadHandler}
+              variant="contained"
+            >
+              Download
+            </Button>
+          </Box>
+          <PassPhraseConfirm
+          open = {open}
+          handleClose = {handleClose}
+          handleConfirm = {handleConfirm}
+          passPhraseConfirmBuffer = {passPhraseConfirmBuffer}
+          />
+        </form>
+        
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TextInput;
+
+
+// working
   // (async () => {
   //   const { message } = await openpgp.encrypt({
   //     message: openpgp.message.fromBinary(new Uint8Array([0x01, 0x01, 0x01])), // input as Message object
@@ -100,46 +181,3 @@ const TextInput = (props) => {
   // console.log(myFile);
   // console.log("myFile");
   // console.log(output)
-  return (
-    <Grid container wrap="nowrap" spacing={0}>
-      <Grid item></Grid>
-      <Grid item xs>
-        <Typography className={classes.heading} variant="h5" gutterBottom>
-          In-Line Pass Phrase
-        </Typography>
-
-        <form onSubmit={inLineAesSubmit}>
-          <Box mb={2}>
-            <TextField
-              onChange={handlePassPhrase}
-              className={classes.pwText}
-              id="pw-in"
-              label="Pass Phrase"
-              variant="outlined"
-            />
-          </Box>
-          <Box mb={2} mr={2}>
-            <TextField
-              className={classes.textBox}
-              fullWidth={true}
-              id="outlined-multiline-static"
-              label="AES 256"
-              multiline
-              rows={10}
-              onChange={handleInput}
-              variant="outlined"
-            />
-          </Box>
-          <Button className={classes.button} type="submit" variant="contained">
-            Default
-          </Button>
-          <Button className={classes.button} onClick = {downloadHandler} variant="contained">
-            download
-          </Button>
-        </form>
-      </Grid>
-    </Grid>
-  );
-};
-
-export default TextInput;
