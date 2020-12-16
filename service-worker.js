adf5f152232f470f318f7352519db2ecca4865f2
shawn <shawnh87@gmail.com>
2020-12-15 21:47:35 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pretty_easy_privacy/precache-manifest.0e72e583a2287c05849316dbc9452b03.js"
+  "/pretty_easy_privacy/precache-manifest.6dc2e278aafb8a77f025f983646b4096.js"
 );
 
 self.addEventListener('message', (event) => {
